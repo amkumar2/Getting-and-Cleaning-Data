ae0186b8b0a4e945dfd60873ae92452079161518
Amit Kumar <amitk64@yahoo.co.in>
2015-04-10 22:50:24 -0400

    assignment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Getting-and-Cleaning-Data
+This file describes how run_analysis.R script works.
+
+1.  download data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  and unzip it
+2. Please extracted folder in Training/Getting-and-cleaning-data  folder of coursera
+3. Created script named run_analysis.R unsing RStudio and saved in Getting-and-cleaning-data folder where files from step#2 kept.
+4. Run the run_analysis.R from RGUI
+5. 2 files are created in default folder.
+6. Files manually moved to Training/Getting-and-cleaning-data
---
+++ run_analysis.R
@@ -0,0 +1,64 @@
+trainData <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/train/X_train.txt")
+dim(trainData) 
+head(trainData)
+trainLabel <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/train/y_train.txt")
+table(trainLabel)
+trainSubject <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/train/subject_train.txt")
+testData <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/test/X_test.txt")
+dim(testData) 
+testLabel <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/test/y_test.txt") 
+table(testLabel) 
+testSubject <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/test/subject_test.txt")
+joinData <- rbind(trainData, testData)
+dim(joinData) 
+joinLabel <- rbind(trainLabel, testLabel)
+dim(joinLabel) 
+joinSubject <- rbind(trainSubject, testSubject)
+dim(joinSubject) 
+
+
+features <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/features.txt")
+dim(features)  
+meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+length(meanStdIndices) 
+joinData <- joinData[, meanStdIndices]
+dim(joinData) 
+names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) 
+names(joinData) <- gsub("mean", "Mean", names(joinData)) 
+names(joinData) <- gsub("std", "Std", names(joinData)) 
+names(joinData) <- gsub("-", "", names(joinData)) 
+
+
+activity <- read.table("C:/Documents and Settings/amkumar2/Training/Getting-and-Cleaning-Data/UCI HAR Dataset/activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+activityLabel <- activity[joinLabel[, 1], 2]
+joinLabel[, 1] <- activityLabel
+names(joinLabel) <- "activity"
+
+
+names(joinSubject) <- "subject"
+cleanedData <- cbind(joinSubject, joinLabel, joinData)
+dim(cleanedData) 
+write.table(cleanedData, file = "merged_data.txt", row.names = FALSE) 
+
+subjectLen <- length(table(joinSubject)) 
+activityLen <- dim(activity)[1] 
+columnLen <- dim(cleanedData)[2]
+result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(cleanedData)
+row <- 1
+for(i in 1:subjectLen) {
+        for(j in 1:activityLen) {
+                result[row, 1] <- sort(unique(joinSubject)[, 1])[i]
+                result[row, 2] <- activity[j, 2]
+                bool1 <- i == cleanedData$subject
+                bool2 <- activity[j, 2] == cleanedData$activity
+                result[row, 3:columnLen] <- colMeans(cleanedData[bool1&bool2, 3:columnLen])
+                row <- row + 1
+        }
+}
+head(result)
+write.table(result, file = "data_with_means.txt",row.names = FALSE) 
